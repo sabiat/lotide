dda20675bbc9fec9abd4f2605e495393c0e61c0d
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-04 23:43:06 +0000

    Implemented head function to return head of array

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// FUNCTION IMPLEMENTATION
+
+const head = function(array) {
+  return array[0];
+};
+
+
+//TEST CODE
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+console.log(head([]));
