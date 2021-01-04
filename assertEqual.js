c312f51faf7d604b8e31154929f4d32297e7ea12
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-04 23:09:30 +0000

    Created assertEqual function to compare 2 values and print message with emojis if it passes/fails

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: " + actual + ' === ' + expected);
+  } else {
+    console.log("🚫🚫🚫Assertion Failed: " +  actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1,2);
+
