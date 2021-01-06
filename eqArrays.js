97a75724165488a8991e2d8f13cf4de22883bf5f
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-06 02:46:24 +0000

    Added condition to ensure array lengths are equal

--- eqArrays.js
+++ eqArrays.js
@@ -7,7 +7,7 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (typeof arr1 !== 'object' || typeof arr2 !== 'object') {
+  if (typeof arr1 !== 'object' || typeof arr2 !== 'object' || arr1.length !== arr2.length) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
