082948188a455663100e5cd1b3de4860631b203f
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-06 20:45:57 +0000

    Implemented countLetters function to return count of letters in sentence

---
+++ countLetters.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const count = {};
+  // const words = sentence.split(' ');
+  // console.log(words);
+
+  for (const letter of sentence) {
+    if (letter === " ") {
+    } else if (count[letter]) {
+      count[letter] += 1;
+    } else {
+      count[letter] = 1;
+    }
+  }
+  return count;
+};
+
+console.log(countLetters('lighthouse in the house'));
+
+const result1 = countLetters('lighthouse in the house');
+const result2 = countLetters('testing     sentence');
+
+assertEqual(result1["h"], 4);
+assertEqual(result2[' '], undefined);
+assertEqual(result1["i"], 2);
