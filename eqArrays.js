bec30ba0adb2b5a4ae33fb33ee47aa591aeb3ad3
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-06 01:23:29 +0000

    Edited code to comply with more test cases

--- eqArrays.js
+++ eqArrays.js
@@ -7,17 +7,22 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
+  if (typeof arr1 !== 'object' || typeof arr2 !== 'object') {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
+  
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 5], [1, 4, 6]), false);
-assertEqual(eqArrays([],[]), true);
+assertEqual(eqArrays([], 1), false);
+assertEqual(eqArrays([], []), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays(['test', 'string'], ['test', 'string']), true);
