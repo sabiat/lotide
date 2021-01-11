f8848884ef959f09c1716e9e9fe4c9cd324a39eb
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-11 17:33:06 +0000

    Refactored assertEqual and head functions and created test subdirectory for test cases

--- assertEqual.js
+++ assertEqual.js
@@ -7,10 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1,2);
-
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,20 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 // FUNCTION IMPLEMENTATION
 
 const head = function(array) {
   return array[0];
 };
 
-
-//TEST CODE
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-console.log(head([]));
+module.exports = head;
