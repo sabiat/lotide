757fdc984ed80a7207b4df3b1cddacd28583ba0d
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-11 20:43:28 +0000

    Refactored countOnly function

--- countOnly.js
+++ countOnly.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 //allItems: an array of strings that we need to look through
 //itemsToCount: an object specifying what to count
 
 //This function will return object with the number of instances of a string for a given array
-
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
@@ -26,21 +17,7 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
 
-const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false});
+module.exports = countOnly;
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+
