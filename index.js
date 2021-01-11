54c4ad3312dc1fffcdffde1edc07f13c2de53a89
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-11 20:19:03 +0000

    Added index.js file to export utility functionsas 1 object

---
+++ index.js
@@ -0,0 +1,27 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const findKey = require('./findKey');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const without = require('./without');
+const takeUntil = require('./takeUntil')
+const findKeyByValue = require('./findKeyByValue');
+
+
+module.exports = {
+  head: head,
+  tail: tail, 
+  middle: middle,
+  map: map,
+  letterPositions: letterPositions,
+  findKey: findKey,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  without: without,
+  takeUntil: takeUntil,
+  findKeyByValue: findKeyByValue,
+};
+
